8e91528779f160b3262ee1038071e6fb1911485d
ElizPN <pankevych.liza@gmail.com>
2023-03-02 15:46:11 +0100

    add program list logic

--- src/components/ProgramsList.tsx
+++ src/components/ProgramsList.tsx
@@ -1,47 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { RadioItem } from "./RadioItem";
 
 // const apiDomain = "https://api.sr.se/api/v2/programs/?format=json&index?programcategoryid=133"
 // const apiDomain = "https://api.sr.se/api/v2/programs/index?programcategoryid=133"
 
+export interface ProgramData {
+  name: string;
+  description: string;
+  programimage: string;
+}
+
 const fetchRadioData = async () => {
   const dataUrl =
-    "https://api.sr.se/api/v2/programs/?format=json&index?programcategoryid=133";
+    "https://api.sr.se/api/v2/programs/?format=json&index?channelid=164&programcategoryid=133";
   return fetch(dataUrl).then((response) => {
-    // console.log(response.json());
-
     return response.json();
   });
 };
 
 export function ProgramsList() {
-  const [radioList, setRadioList] = useState([]);
+  const [radioList, setRadioList] = useState<ProgramData[]>([]);
 
   const handleOnclick = async () => {
-    try {
-      const radioData = await fetchRadioData();
-      const programs = radioData.programs;
+    const radioData = await fetchRadioData();
+    const programs = radioData.programs;
 
-      const programsInfo = programs.map(
-        (item: { name: string; description: string; programimage: string }) => {
-          const progaramdata = {
-            name: item.name,
-            description: item.description,
-            programimage: item.programimage,
-          };
-
-          return progaramdata;
-        }
-      );
-      console.log(programsInfo);
-      
-
-    //   console.log(programs);
-    } catch (error) {
-      console.log(error);
-    }
+    const programsInfo = programs.map((item: ProgramData) => {
+      const progaramdata = {
+        name: item.name,
+        description: item.description,
+        programimage: item.programimage,
+      };
+      return progaramdata;
+    });
+    setRadioList(programsInfo);
   };
 
-  //   console.log(radioData);
+  return (
+    <div>
+      {/* {radioList.length > 0 && (
+        <ul>
+          <li>{radioList[0].name}</li>
+          <li>
+            <img src={radioList[0].programimage} />
+          </li>
+          <li>{radioList[0].description}</li>
+        </ul>
+      )} */}
+      <button onClick={handleOnclick}>start</button>
 
-  return <button onClick={handleOnclick}>start</button>;
+      {radioList.length > 0 &&
+        radioList.map((item, index) => {
+          return <RadioItem item={item}></RadioItem>;
+        })}
+    </div>
+  );
 }
