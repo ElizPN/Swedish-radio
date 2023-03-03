6d168ef030a2c821ee4166e04d7075d28927bfe1
ElizPN <pankevych.liza@gmail.com>
2023-03-03 09:09:15 +0100

    add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { ProgramsList } from "./components/ProgramsList";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <ProgramsList />
-    </div>
+    </ThemeProvider>
   );
 }
 
