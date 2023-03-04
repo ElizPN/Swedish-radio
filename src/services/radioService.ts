63f9dc3f1c6b22437d2bb38a5a86f05ddf2b9812
ElizPN <pankevych.liza@gmail.com>
2023-03-04 15:10:17 +0100

    change id type

--- src/services/radioService.ts
+++ src/services/radioService.ts
@@ -1,4 +1,4 @@
-export const fetchRadioData = async (id: number) => {
+export const fetchRadioData = async (id: string) => {
   const dataUrl = `https://api.sr.se/api/v2/programs/index?programcategoryid=${id}&format=JSON&pagination=false`;
   return fetch(dataUrl).then((response) => {
     return response.json();
