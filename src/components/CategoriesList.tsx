0b7777fc52400ef28993a752e379ca840910d13b
ElizPN <pankevych.liza@gmail.com>
2023-03-04 15:11:11 +0100

    pass state to ather component

--- src/components/CategoriesList.tsx
+++ src/components/CategoriesList.tsx
@@ -1,35 +1,48 @@
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import Box from "@mui/system/Box";
 import { nanoid } from "nanoid";
 import { useState } from "react";
 import { CategoryItem } from "./CategoriesController";
+import { ProgramsList } from "./ProgramsList";
 
 interface CategoriesListProps {
   categoryList: CategoryItem[];
 }
 
+interface Option {
+  name: string;
+  id: number;
+}
+const defaultOption: Option = {
+  name: "",
+  id: 0,
+};
+
 export function CategoriesList({ categoryList }: CategoriesListProps) {
   const [selectedOption, setSelectedOption] = useState("");
 
   return (
-    <FormControl>
-      <InputLabel id='demo-simple-select-label'>Category</InputLabel>
-      <Select
-        value={selectedOption}
-        onChange={(event) => setSelectedOption(event.target.value)}
-        labelId='demo-simple-select-label'
-        id='demo-simple-select'
-        label='Age'
-      >
-        {categoryList.length > 0 &&
-          categoryList.map((item) => {
-            return (
-              <MenuItem key={nanoid()} value={item.name}>
-                {item.name}
-                {item.id}
-              </MenuItem>
-            );
-          })}
-      </Select>
-    </FormControl>
+    <Box>
+      <FormControl>
+        <InputLabel id='demo-simple-select-label'>Category</InputLabel>
+        <Select
+          value={selectedOption}
+          onChange={(event) => setSelectedOption(event.target.value)}
+          labelId='demo-simple-select-label'
+          id='demo-simple-select'
+          label='Age'
+        >
+          {categoryList.length > 0 &&
+            categoryList.map((item) => {
+              return (
+                <MenuItem key={nanoid()} value={item.id}>
+                  {item.name}
+                </MenuItem>
+              );
+            })}
+        </Select>
+      </FormControl>
+      <ProgramsList selectedOption={selectedOption} />
+    </Box>
   );
 }
