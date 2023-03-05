2918d91ff4202751ceb95d15aa74997834152601
ElizPN <pankevych.liza@gmail.com>
2023-03-05 13:27:49 +0100

    move combine handler for select categories

--- src/components/CategoriesList.tsx
+++ src/components/CategoriesList.tsx
@@ -4,22 +4,41 @@ import { nanoid } from "nanoid";
 import { useState } from "react";
 import { CategoryItem } from "./CategoriesController";
 import { ProgramsList } from "./ProgramsList";
+import { fetchRadioData } from "../services/radioService";
 
 interface CategoriesListProps {
   categoryList: CategoryItem[];
 }
-
-interface Option {
+export interface ProgramData {
   name: string;
-  id: number;
+  description: string;
+  programimage: string;
+  programurl: string;
 }
-const defaultOption: Option = {
-  name: "",
-  id: 0,
-};
 
 export function CategoriesList({ categoryList }: CategoriesListProps) {
+  const [radioList, setRadioList] = useState<ProgramData[]>([]);
   const [selectedOption, setSelectedOption] = useState("");
+
+  // React.ChangeEvent<HTMLSelectElement>
+
+  const handleOnchange = async (event: any) => {
+    const radioData = await fetchRadioData(event.target.value);
+    const programs = radioData.programs;
+
+    const programsInfo = programs.map((item: ProgramData) => {
+      const progaramdata = {
+        name: item.name,
+        description: item.description,
+        programimage: item.programimage,
+        programurl: item.programurl,
+      };
+
+      return progaramdata;
+    });
+    setSelectedOption(event.target.value);
+    setRadioList(programsInfo);
+  };
 
   return (
     <Box>
@@ -27,7 +46,7 @@ export function CategoriesList({ categoryList }: CategoriesListProps) {
         <InputLabel id='demo-simple-select-label'>Category</InputLabel>
         <Select
           value={selectedOption}
-          onChange={(event) => setSelectedOption(event.target.value)}
+          onChange={handleOnchange}
           labelId='demo-simple-select-label'
           id='demo-simple-select'
           label='Age'
@@ -42,7 +61,7 @@ export function CategoriesList({ categoryList }: CategoriesListProps) {
             })}
         </Select>
       </FormControl>
-      <ProgramsList selectedOption={selectedOption} />
+      <ProgramsList selectedOption={selectedOption} radioList={radioList} />
     </Box>
   );
 }
--- src/components/ProgramsList.tsx
+++ src/components/ProgramsList.tsx
@@ -1,41 +1,21 @@
-import { Box, Button } from "@mui/material";
-import { useState } from "react";
-import { fetchRadioData } from "../services/radioService";
+import { Box } from "@mui/material";
+import { ProgramData } from "./CategoriesList";
 import { RadioItem } from "./RadioItem";
 
-export interface ProgramData {
-  name: string;
-  description: string;
-  programimage: string;
-  programurl: string;
-}
+
 interface ProgramsListProps {
   selectedOption: string;
+  radioList: ProgramData[];
 }
 
-export function ProgramsList({ selectedOption }: ProgramsListProps) {
-  const [radioList, setRadioList] = useState<ProgramData[]>([]);
-
-  const handleOnclick = async () => {
-    const radioData = await fetchRadioData(selectedOption);
-    const programs = radioData.programs;
-
-    const programsInfo = programs.map((item: ProgramData) => {
-      const progaramdata = {
-        name: item.name,
-        description: item.description,
-        programimage: item.programimage,
-        programurl: item.programurl,
-      };
-
-      return progaramdata;
-    });
-    setRadioList(programsInfo);
-  };
+export function ProgramsList({
+ 
+  radioList,
+}: ProgramsListProps) {
+  
 
   return (
     <Box>
-      <Button onClick={handleOnclick}>start</Button>
       <RadioItem radioList={radioList} />
     </Box>
   );
