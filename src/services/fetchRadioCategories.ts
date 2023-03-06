772038daa9fcb6552e2709d30ce698895513a85d
ElizPN <ElizPN@users.noreply.github.com>
2023-03-06 15:09:02 +0000

    deploy: fbf9745027076fc65a9f7655728e8aa629ab6d40

--- src/services/fetchRadioCategories.ts
+++
@@ -1,7 +0,0 @@
-export const fetchRadioCategories = async () => {
-  const dataUrl =
-    "https://api.sr.se/api/v2/programcategories/?format=JSON&pagination=false";
-  const response = await fetch(dataUrl);
-  const data = await response.json();
-  return data.programcategories;
-};
--- src/services/fetchRadioPrograms.ts
+++
@@ -1,8 +0,0 @@
-export const fetchRadioData = async (id: string) => {
-  const dataUrl = `https://api.sr.se/api/v2/programs/index?programcategoryid=${id}&format=JSON&pagination=false`;
-  return fetch(dataUrl).then((response) => {
-    return response.json();
-  });
-};
-
-
