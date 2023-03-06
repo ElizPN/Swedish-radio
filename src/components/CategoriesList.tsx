036e639ddeb3695c9bcacd29335e761c12d78a96
ElizPN <pankevych.liza@gmail.com>
2023-03-06 12:01:53 +0100

    fix styles, add grid

--- src/components/CategoriesList.tsx
+++ src/components/CategoriesList.tsx
@@ -1,4 +1,12 @@
-import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import {
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
 import Box from "@mui/system/Box";
 import { nanoid } from "nanoid";
 import { useState } from "react";
@@ -16,10 +24,16 @@ export interface ProgramData {
   programurl: string;
 }
 
+const styleGridContainer = {
+  marginTop: 5,
+  marginLeft: 1,
+  width: { xs: "90%", md: "50%", lg: "35%" },
+  padding: 1,
+};
+
 export function CategoriesList({ categoryList }: CategoriesListProps) {
   const [radioList, setRadioList] = useState<ProgramData[]>([]);
   const [selectedOption, setSelectedOption] = useState("");
-
 
   const handleOnchange = async (event: SelectChangeEvent<string>) => {
     const radioData = await fetchRadioData(event.target.value);
@@ -41,25 +55,42 @@ export function CategoriesList({ categoryList }: CategoriesListProps) {
 
   return (
     <Box>
-      <FormControl>
-        <InputLabel id='demo-simple-select-label'>Category</InputLabel>
-        <Select
-          value={selectedOption}
-          onChange={handleOnchange}
-          labelId='demo-simple-select-label'
-          id='demo-simple-select'
-          label='Age'
-        >
-          {categoryList.length > 0 &&
-            categoryList.map((item) => {
-              return (
-                <MenuItem key={nanoid()} value={item.id}>
-                  {item.name}
-                </MenuItem>
-              );
-            })}
-        </Select>
-      </FormControl>
+      <Typography textAlign='center' variant='h3'>
+        Svensk Radioprogram
+      </Typography>
+      <Grid container direction='column' sx={styleGridContainer} spacing={3}>
+        <Grid item xs={6}>
+          <Typography textAlign='center' variant='h5'>
+            Välj en kategori för att se programmen
+          </Typography>
+        </Grid>
+        <Grid item xs='auto'>
+          <FormControl fullWidth={true}>
+            <InputLabel
+              sx={{ backgroundColor: "black", pr: "5px" }}
+              id='demo-simple-select-label'
+            >
+              Category
+            </InputLabel>
+            <Select
+              value={selectedOption}
+              onChange={handleOnchange}
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
+              label='Age'
+            >
+              {categoryList.length > 0 &&
+                categoryList.map((item) => {
+                  return (
+                    <MenuItem key={nanoid()} value={item.id}>
+                      {item.name}
+                    </MenuItem>
+                  );
+                })}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
       <ProgramsList selectedOption={selectedOption} radioList={radioList} />
     </Box>
   );
