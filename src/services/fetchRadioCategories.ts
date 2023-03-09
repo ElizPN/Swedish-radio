79cc823306b48891b9dae433d83bedb1d7f90233
ElizPN <pankevych.liza@gmail.com>
2023-03-09 15:56:39 +0100

    clean up

--- src/services/fetchRadioCategories.ts
+++ src/services/fetchRadioCategories.ts
@@ -3,5 +3,6 @@ export const fetchRadioCategories = async () => {
     "https://api.sr.se/api/v2/programcategories/?format=JSON&pagination=false";
   const response = await fetch(dataUrl);
   const data = await response.json();
+
   return data.programcategories;
 };
