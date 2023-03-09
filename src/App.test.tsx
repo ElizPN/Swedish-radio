54cf7cf5a54467d14c0bd1b558667c8f2a93e307
ElizPN <pankevych.liza@gmail.com>
2023-03-09 15:55:56 +0100

    fix title

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders title ", () => {
   render(<App />);
   const linkElement = screen.getByText(/Sveriges Radio program/i);
   expect(linkElement).toBeInTheDocument();
